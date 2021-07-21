951edb4836907a973acde0e9c2c3a8a36ee176bd
IvayloSilovski1 <ivaylo.silovski689@gmail.com>
2021-07-22 01:42:40 +0300

    Sockets ready

--- frontend/src/components/CoursesCard.js
+++ frontend/src/components/CoursesCard.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Avatar, Card, CardHeader, Typography } from '@material-ui/core';
 
-const CoursesCard = () => {
-  return <div></div>;
+const CoursesCard = ({ course }) => {
+  return (
+    <Card>
+      <CardHeader
+        avatar={<Avatar />}
+        title={<Typography variant='h6'>{course.name}</Typography>}
+      />
+    </Card>
+  );
 };
 
 export default CoursesCard;
--- frontend/src/pages/CoursesPage.js
+++ frontend/src/pages/CoursesPage.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import {
+  CircularProgress,
+  Container,
+  Grid,
+  makeStyles,
+} from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import CoursesCard from './../components/CoursesCard';
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: 20,
+  },
+  loader: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paper: {
+    marginBottom: '1rem',
+    padding: '13px',
+  },
+  filters: {
+    padding: '0 1.5rem',
+  },
+  priceRangeInputs: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
 
 const CoursesPage = () => {
+  const classes = useStyles();
+
   // component state
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -35,10 +65,23 @@ const CoursesPage = () => {
   }, []);
 
   return (
-    <Container>
+    <Container className={classes.root}>
       {/* 1. Filter and sort data */}
 
       {/* 2. List the data */}
+      <Grid container spacing={2}>
+        {loading ? (
+          <div className={classes.loader}>
+            <CircularProgress size='4rem' thickness={5} />
+          </div>
+        ) : (
+          courses.map((course) => (
+            <Grid item key={course._id} xs={12} sm={6} md={4} lg={3}>
+              <CoursesCard course={course} />
+            </Grid>
+          ))
+        )}
+      </Grid>
     </Container>
   );
 };
